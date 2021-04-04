c2b5a263a8427a22c79859964b81fa44bdc8c225
ja645 <tbzgkzejyk1ef5s91wuf@gmail.com>
2021-04-04 18:22:28 +0900

    added forms birthday, sex, former_job, job, school_id to signup-page

--- resources/views/auth/user/create.blade.php
+++ resources/views/auth/user/create.blade.php
@@ -19,6 +19,41 @@
                 <x-input id="name" class="block mt-1 w-full" type="text" name="name" required autofocus />
             </div>
 
+            <!-- Birthday -->
+            <div>
+                <x-label for="birthday" :value="__('Birthday')" />
+
+                <x-input id="birthday" class="block mt-1 w-full" type="text" name="birthday" required autofocus />
+            </div>
+
+            <!-- Sex -->
+            <div>
+                <x-label for="sex" :value="__('Sex')" />
+
+                <x-input id="sex" class="block mt-1 w-full" type="text" name="sex" required autofocus />
+            </div>
+
+            <!-- Former_job -->
+            <div>
+                <x-label for="former_job" :value="__('Former_job')" />
+
+                <x-input id="former_job" class="block mt-1 w-full" type="text" name="former_job" required autofocus />
+            </div>
+
+            <!-- Job -->
+            <div>
+                <x-label for="job" :value="__('Job')" />
+
+                <x-input id="job" class="block mt-1 w-full" type="text" name="job" required autofocus />
+            </div>
+
+            <!-- School_id -->
+            <div>
+                <x-label for="school_id" :value="__('School_id')" />
+
+                <x-input id="school_id" class="block mt-1 w-full" type="text" name="school_id" required autofocus />
+            </div>
+
             <!-- Email Address -->
             <div class="mt-4">
                 <x-label for="email" :value="__('Email')" />
