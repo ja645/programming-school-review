e1c5427bb696c7c18e52e349a4695a76ede6db02
ja645 <tbzgkzejyk1ef5s91wuf@gmail.com>
2021-04-04 13:14:05 +0900

    added test every field is required

--- tests/Unit/UserFormRequestTest.php
+++ tests/Unit/UserFormRequestTest.php
@@ -34,10 +34,45 @@ class UserFormRequestTest extends TestCase
     public function dataUserRegistration()
     {
         return [
-            '必須エラー' => [
+            'OK' => [
                 ['user_name', 'birthday', 'sex', 'former_job', 'job', 'school_id', 'email', 'password'],                        
-                ['abc', 20090530, 1, null, null, 1, 'abcd@example.com', 'abc3def'],
+                ['田中 太郎', "2013-5-30 00:00:00.000000", 1, null, null, 1, 'abcd@example.com', 'password'],
                 true
+            ],
+            'OK2' => [
+                ['user_name', 'birthday', 'sex', 'former_job', 'job', 'school_id', 'email', 'password'],                        
+                ['田中 太郎', "2013-5-30 00:00:00.000000", 1, '公務員', 'エンジニア', 1, 'abcd@example.com', 'password'],
+                true
+            ],
+            'user_nameが空でエラー' =>[
+                ['user_name', 'birthday', 'sex', 'former_job', 'job', 'school_id', 'email', 'password'],                        
+                [null, '2013-5-30 00:00:00.000000', 1, null, null, 1, 'abcd@example.com', 'password'],
+                false
+            ],
+            'birthdayが空でエラー' =>[
+                ['user_name', 'birthday', 'sex', 'former_job', 'job', 'school_id', 'email', 'password'],                        
+                ['田中 太郎', null, 1, null, null, 1, 'abcd@example.com', 'password'],
+                false
+            ],
+            'sexが空でエラー' =>[
+                ['user_name', 'birthday', 'sex', 'former_job', 'job', 'school_id', 'email', 'password'],                        
+                ['田中 太郎', '2013-5-30 00:00:00.000000', null, null, null, 1, 'abcd@example.com', 'password'],
+                false
+            ],
+            'school_idが空でエラー' =>[
+                ['user_name', 'birthday', 'sex', 'former_job', 'job', 'school_id', 'email', 'password'],                        
+                ['田中 太郎', '2013-5-30 00:00:00.000000', 1, null, null, null, 'abcd@example.com', 'password'],
+                false
+            ],
+            'emailが空でエラー' =>[
+                ['user_name', 'birthday', 'sex', 'former_job', 'job', 'school_id', 'email', 'password'],                        
+                ['田中 太郎', '2013-5-30 00:00:00.000000', 1, null, null, 1, null, 'password'],
+                false
+            ],
+            'passwordが空でエラー' =>[
+                ['user_name', 'birthday', 'sex', 'former_job', 'job', 'school_id', 'email', 'password'],                        
+                ['田中 太郎', '2013-5-30 00:00:00.000000', 1, null, null, 1, 'abcd@example.com', null],
+                false
             ],
         ];
     }
