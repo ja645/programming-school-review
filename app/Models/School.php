f43d2843448f91115881466e4cc0be77f941fc67
ja645 <tbzgkzejyk1ef5s91wuf@gmail.com>
2021-04-14 17:05:18 +0900

    added School model

---
+++ app/Models/School.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class School extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass nonassignable.
+     *
+     * @var array
+     */
+    protected $guarded = [
+        'id',
+    ];
+
+    /**
+     * スクールに投稿されたレビューを取得
+     */
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+}
