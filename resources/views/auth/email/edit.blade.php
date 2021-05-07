d99ab4c13d92dc0baf8be1f4a5dd4cebeae0296d
ja645 <tbzgkzejyk1ef5s91wuf@gmail.com>
2021-05-07 11:10:35 +0900

    created email.edit.blade

--- resources/views/auth/email/edit.blade.php
+++ resources/views/auth/email/edit.blade.php
@@ -1,12 +1,98 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>@yield('title')</title>
+
+  <!-- Fonts -->
+  <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+  
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <script>
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+      return new bootstrap.Popover(popoverTriggerEl)
+    })
+  </script>
+  <!-- Styles -->
+  <!-- ローカルではsecure_assetを使わない -->
+  @if(app('env')=='local')
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/admin.css') }}" rel="stylesheet">
+  @endif
+  @if(app('env')=='production')
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet">
+  @endif
+  <!-- 個別のcssを読み込む -->
+  @stack('css')
+
+  <!-- FontAwesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
 </head>
 <body>
+
+<div class="container-xl">
+
+  <div class="row d-flex justify-content-center">
+
+    <div class="col-md-8">
+
+        <div name="logo">
+            <a href="/">
+                ロゴ
+            </a>
+        </div>
+
+        <!-- Session Status -->
+        <div class="mb-4" :status="session('status')"></div>
+
+        <!-- Validation Errors -->
+        <div class="mb-4" :errors="$errors"></div>
+
+        <form method="POST" action="{{ url('/email/edit') }}">
+          @csrf
+
+          <div id="user-prof" class="card" style="margin: 10.0rem 0;">
+            <div class="card-header">メールアドレス変更</div>
+            <div class="card-body text-secondary">
+              <ul class="list-group list-group-flush">
+                
+                <li class="list-group-item">
+                    <label for="new_email">新しいメールアドレス</label>
+                    <input type="email" name="new_email" value="{{ old('new_email', '新しいメールアドレスを入力してください。') }}">
+                </li>
+               
+                <li class="list-group-item">
+                  <button type="submit" class="btn btn-success" data-bs-container="body">
+                      メールアドレスを変更する
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+        </form>
+    </div>
+  </div>
+</div>
+
+<!-- Scripts -->
+@if(app('env')=='local')
+  <script src="{{ asset('js/app.js') }}" defer></script>
+  @endif
+  @if(app('env')=='production')
+  <script src="{{ secure_asset('js/app.js') }}" defer></script>
+  @endif
   
+  <!-- Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </body>
 </html>
