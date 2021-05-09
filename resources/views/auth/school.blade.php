af5cf56931c782bbbc13742e482f6dd15f8fe569
ja645 <tbzgkzejyk1ef5s91wuf@gmail.com>
2021-05-09 10:19:44 +0900

    changed like-component to inline-block

--- resources/views/auth/school.blade.php
+++ resources/views/auth/school.blade.php
@@ -8,7 +8,7 @@
     <div class="col-11 mx-auto school-head-card">
       <div class="row school-head">
         <div class="col-sm-1 d-none d-sm-block"></div>
-        <div class="school-name col text-sm-start text-center px-0">{{ $school->school_name }}<span id="like"><like-component :school="{{ json_encode($school) }}"></like-component></span></div>
+        <div class="school-name col text-sm-start text-center px-0">{{ $school->school_name }}<span id="like"><like-component :school="{{ json_encode($school) }}"  style="display: inline-block;"></like-component></span></div>
       </div>
       <div class="row">
         <div class="col-sm-1 col-2"></div>
