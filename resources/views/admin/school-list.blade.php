5fef6fb9a92ef2298cf016f31dee326056e97c3b
ja645 <tbzgkzejyk1ef5s91wuf@gmail.com>
2021-05-19 16:05:17 +0900

    fixed admin.school-list.blade

--- resources/views/admin/school-list.blade.php
+++ resources/views/admin/school-list.blade.php
@@ -12,14 +12,18 @@
 
     <div class="review-list">
       @foreach ($schools as $school)
-      <a href="{{ url('/schools/' . $school->id) }}"  class="list-group-item list-group-item-action">
-        {{ $school->school_name }}
-        <a href="/admin/edit">編集</a>
-        <form id="admin-delete" action="'/admin/delete" method="post">
-          @csrf
-          <a href="/admin/delete" onclick="event.preventDefault(); document.getElementById('admin-delete').submit();">削除</a>
-        </form>
-      </a>
+      <div class="list-group-item list-group-item-action d-flex justify-content-between">
+        <a href="{{ url('/schools/' . $school->id) }}">
+          {{ $school->school_name }}
+        </a>
+          <div class="d-flex">
+            <a href="/admin/edit">編集</a>
+            <form id="admin-delete" action="'/admin/delete" method="post">
+            @csrf
+              <a href="/admin/delete" onclick="event.preventDefault(); document.getElementById('admin-delete').submit();">削除</a>
+            </form>
+          </div>
+      </div>
       @endforeach
     </div>
   @endif
