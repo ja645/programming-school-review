6b50870f1d79db7d965015ba12a8dc29734d2976
ja645 <tbzgkzejyk1ef5s91wuf@gmail.com>
2021-05-22 04:39:25 +0900

    created ReviewSeeder

---
+++ database/seeders/ReviewSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Review;
+use App\Models\User;
+use App\Models\School;
+
+class ReviewSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 1; $i <= 20; $i++) {
+            $user = User::find($i);
+    
+            $school = School::find($i);
+            Review::factory()->for($user)->for($school)->count(10)->create();
+        }
+    }
+}
